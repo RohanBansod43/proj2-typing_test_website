b82fd2785a006cbc1e1996f5964c2eb3bc504f7d
Rohan <rohanbansod60@gmail.com>
2024-01-13 00:23:07 +0530

    added key press events

--- index.js
+++ index.js
@@ -21,25 +21,45 @@ let final_sentence;
 function getRandomWord() {
   let randomNumber = Math.floor(Math.random() * lenWordBank);
   randomWord = word_bank[randomNumber];
-  console.log(randomWord);
+
   return randomWord;
 }
 // make a random sentence from randowm words from array
-function makeSentence(words){
-    let temp_word;
-    for(let i = 0; i<words; i++){
-        if (i==0){
-            final_sentence = getRandomWord();
-        }
-        else if (i>0){
-            temp_word = getRandomWord()
-            final_sentence = final_sentence + " " + temp_word;
-        }
-        else{
-            console.log("hola");
-        }
+function makeSentence(words) {
+  let temp_word;
+  for (let i = 0; i < words; i++) {
+    if (i == 0) {
+      final_sentence = getRandomWord();
+    } else if (i > 0) {
+      temp_word = getRandomWord();
+      final_sentence = final_sentence + " " + temp_word;
+    } else {
+      console.log("hola");
     }
-    return final_sentence;
+  }
+  return final_sentence;
 }
 
-document.querySelector(".text").innerHTML= makeSentence(30);
+let keyDownCount = 0;
+let sentence = makeSentence(30);
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+
+  if (event.key == sentence[keyDownCount]) {
+    document.querySelector(".text").classList.add("sucess");
+    console.log(sentence[keyDownCount]);
+    console.log("ok");
+    keyDownCount++;
+    console.log(keyDownCount);
+  } else if (event.key == backspace) {
+    console.log("ok");
+    console.log(keyDownCount);
+  } else {
+    console.log(sentence[keyDownCount]);
+    console.log("not ok");
+    keyDownCount++;
+    console.log(keyDownCount);
+  }
+});
+
+document.querySelector(".text").innerHTML = sentence;
